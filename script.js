6caaadd107ba8859d53dff909bb6dd70385ce82d
Wicher <wicherICT@hotmail.com>
2023-10-23 16:59:25 +0200

    Initial commit.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//Alternatieve methoden van elementen selecten bij id.
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+//Set initial values
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
